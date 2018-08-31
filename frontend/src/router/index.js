7687d2d97322d5a6b03712689d143d9028d173c5
gleissongtr <gleissongtr@gleissongtr-pc>
2018-08-30 23:47:24 -0300

    routing to ex01

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Ex01 from '@/components/Ex01'
 
 Vue.use(Router)
-
+// https://www.youtube.com/watch?reload=9&v=4lk9-PYensI
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/ex01',
+      name: 'ex01',
+      component: Ex01
     }
   ]
 })
