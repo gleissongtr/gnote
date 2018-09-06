8aa6730500ccacb6a2056fcc99763c407af7c2d3
gleissongtr <gleissongtr@gleissongtr-pc>
2018-09-06 10:18:50 -0300

    for with button types

--- frontend/src/components/Ex01.vue
+++ frontend/src/components/Ex01.vue
@@ -5,7 +5,7 @@
     <div>DESCRIÇÃO: {{gasto.descricao}}</div>
     <div>VALOR: {{gasto.valor}}</div>
     <div>DATA: {{gasto.data}}</div>
-    <button @click="buscar()">asdf</button>
+    <b-button @click="buscar()">buscar</b-button>
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -37,21 +37,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <div class="row">
+      <template v-for="variant in ['primary','secondary','success','outline-success','warning','danger','link']">
+        <div class="col-md-4 pb-2" v-for="size in ['sm','','lg']" :key="`${variant}_${size}`">
+          <b-button :size="size" :variant="variant">
+            {{variant}} {{size}}
+          </b-button>
+        </div>
+      </template>
+    </div>
     <b-form-input v-model="text1"
                   type="text"
                   placeholder="Enter your name"></b-form-input>
