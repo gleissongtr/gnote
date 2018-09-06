cdb86cde7b3908c6124468336f4cd6573c6a5155
gleissongtr <gleissongtr@gleissongtr-pc>
2018-09-06 00:14:22 -0300

    add vue bootstrap

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Ex01 from '@/components/Ex01'
+import Input from '@/components/Input'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/ex01',
       name: 'ex01',
       component: Ex01
+    },
+    {
+      path: '/input',
+      name: 'input',
+      component: Input
     }
   ]
 })
